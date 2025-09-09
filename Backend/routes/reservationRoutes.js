fad777931ce2e226c63ad21c5f10621dcd5b5675
Danish083 <deephundal083@gmail.com>
2025-09-09 20:21:05 +0530

    cors update

--- Backend/routes/reservationRoutes.js
+++ Backend/routes/reservationRoutes.js
@@ -3,6 +3,10 @@ import { sendReservation } from '../controller/reservation.js';
 
 const reservationRouter = Router();
 
+reservationRouter.options('/send', (req, res) => {
+  res.sendStatus(200);
+});
+
 reservationRouter.post('/send', sendReservation );
 
 
